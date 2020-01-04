080fb2309973435d3255e868d06778f8114900c1
Dane <56905214+dfmattern@users.noreply.github.com>
2020-01-04 10:05:12 -0700

    added functions to turn on and off overlays

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,37 +1,67 @@
-$ (document).ready (function() {
+$(function() {
+  //global variables
+  let wins = 0;
+  let losses = 0;
+  let numberToGuess;
+  let playerTotal = 0;
 
+  function reset() {
+    //generate number to guess
+    numberToGuess = Math.floor(Math.random() * 101 + 19);
+    //console.log(numberToGuess);
+    //generate new values for crystals
+    for (var i = 0; i < crystal.length; i++) {
+      crystal[i].attr("crystalValue", Math.floor(Math.random() * 11 + 1));
+    }
+    //reset player score
+    playerTotal = 0;
+    //update total score
+    $("#totalScore").text(playerTotal);
+    //update random number to guess
+    $("#randomNumber").text(numberToGuess);
+  }
 
-//global variables
-let numberToGuess;
-let playerTotal = 0;
-let wins = 0;
-let losses = 0;
-
-//generate number to guess
-numberToGuess = Math.floor(Math.random() * 101 + 19);
-//console.log(numberToGuess);
-playerTotal = 0;
-//update total score
-$("#totalScore").text(playerTotal);
-//update random number to guess
-$("#randomNumber").text(numberToGuess);
-
-
-
-function winloss() {
+  function winloss() {
     if (playerTotal === numberToGuess) {
-        document.getElementById("win-overlay").style.display = "block";
-        reset();
-        wins++;
-        $("#wins").text("Wins:" + wins);
+      //display win overlay
+      document.getElementById("win-overlay").style.display = "block";
+      //update wins total
+      wins++;
+      //update wins display
+      $("#wins").text("Wins:" + wins);
+      //reset game
+      reset();
+    } else if (playerTotal > numberToGuess) {
+      document.getElementById("lose-overlay").style.display = "block";
+      losses++;
+      $("#losses").text("Losses:" + losses);
+      reset();
     }
-    else (playerTotal > numberToGuess){
-        document.getElementById("lose-overlay").style.display = "block";
-        reset();
-        losses++;
-        $("#losses").text("Losses:" + losses);
-    }
+  }
+
+  function winOn() {
+    document.getElementById("win-overlay").style.display  = "block";
+  }
+
+  function winOff() {
+    document.getElementById("win-overlay").style.display = "none";
+  } 
+
+  function loseOn() {
+    document.getElementById("lose-overlay").style.display  = "block";
+  }
+
+  function loseOff() {
+    document.getElementById("lose-overlay").style.display = "none";
+  }
 
 
-}
-})
+
+  function gameStart() {
+    crystal = document.getElementsByClassName("btn");
+    for (var i = 0; i < crystal.length; i++) {
+      crystal[i].attr("crystalValue", Math.floor(Math.random() * 11 + 1));
+      crystal[i].on("click", event);
+    }
+  }
+});
